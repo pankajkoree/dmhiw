520a6fb3d72a421862f1ef0dfd1df1b0f9331ed4
pankajkoree <pankajkori2057@gmail.com>
2025-02-19 23:13:54 +0530

    ai integration api done

--- src/app/api/ai-content/route.ts
+++ src/app/api/ai-content/route.ts
@@ -1,4 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextRequest } from "next/server";
 
 // api key from gemini
 const gemini_api_key = process.env.API_KEY;
@@ -6,17 +7,17 @@ const gemini_api_key = process.env.API_KEY;
 const googleAI = new GoogleGenerativeAI(gemini_api_key as string);
 const geminiModel = googleAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "tell about nextjs";
+// const prompt = "tell about nextjs";
 
-export async function GET(req) {
+export async function POST(request: NextRequest) {
   try {
-    const { question } = await req.json();
+    const { question } = await request.json();
     if (!question) {
       return Response.json({ error: "Question is required" }, { status: 400 });
     }
 
     const result = await geminiModel.generateContent(question);
-    const response = await result.response.text(); // Correct response handling
+    const response = await result.response.text();
 
     return Response.json({ result: response }, { status: 200 });
   } catch (error) {
