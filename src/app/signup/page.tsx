5881546d12d334d09ffd6b90ca6c14b64857b8c1
pankajkoree <pankajkori2057@gmail.com>
2025-02-26 23:35:44 +0530

    signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,30 +1,65 @@
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+"use client";
+
 import React from "react";
 
 const Signup = () => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
   return (
-    <div>
-      {/* username starts here */}
-      <div>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
-      </div>
-      {/* username ends here */}
+    <div className="relative flex justify-center xl:top-20">
+      <div className="relative flex flex-col justify-center items-center border p-2 xl:w-[25%] gap-4 rounded-xl">
+        <div>
+          <h2 className="text-3xl">Signup form</h2>
+          <p className="text-xl">Create your account</p>
+        </div>
 
-      {/* email starts here */}
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="text" id="email" />
-      </div>
-      {/* email ends here */}
+        <form
+          className="relative flex flex-col gap-2 p-2 text-xl xl:w-[90%]"
+          onSubmit={handleSubmit}
+        >
+          {/* username starts here */}
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            className="relative flex h-[36px] border rounded-xl"
+          />
+          {/* username ends here */}
 
-      {/* email starts here */}
-      <div>
-        <label htmlFor="password">Email</label>
-        <input type="text" id="email" />
+          {/* email starts here */}
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            className="relative flex h-[36px] border rounded-xl"
+          />
+          {/* email ends here */}
+
+          {/* passowrd starts here */}
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            className="relative flex h-[36px] border rounded-xl"
+          />
+          <label htmlFor="cnfpassword">Confirm password</label>
+          <input
+            type="password"
+            id="cnfpassword"
+            className="relative flex h-[36px] border rounded-xl"
+          />
+          {/* password ends here */}
+
+          {/* signup button starts here */}
+          <button type="submit" className="border p-2">
+            Signup
+          </button>
+          {/* signup button ends here */}
+        </form>
       </div>
-      {/* email ends here */}
     </div>
   );
 };
