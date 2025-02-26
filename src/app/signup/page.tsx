196d6af9186d9eeb1fb20324c9becb51ab78f8b8
pankajkoree <pankajkori2057@gmail.com>
2025-02-26 21:49:40 +0530

    signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,25 @@
-import SignupFormDemo from "@/components/signup-form-demo";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import React from "react";
 
 const Signup = () => {
   return (
     <div>
-      <SignupFormDemo />
+      {/* username starts here */}
+      <div>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+      </div>
+      {/* username ends here */}
+
+      {/* email starts here */}
+      <div>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+      </div>
+      {/* email ends here */}
+
+
     </div>
   );
 };
