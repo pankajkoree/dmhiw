10bc2f9eac2819325bd3dc24dab84fbcd898b691
pankajkoree <pankajkori2057@gmail.com>
2025-03-27 12:46:02 +0530

    forgot credentials

--- src/app/forgotCredentials/page.tsx
+++ src/app/forgotCredentials/page.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-
 const forgotCredentials = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handleCredentials = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <h1>Forgot username or password</h1>
-      <label id="useroremail">Enter username or email</label>
-      <input type="text" id="useroremail" onChange={} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" />
+      </form>
     </div>
   );
 };
