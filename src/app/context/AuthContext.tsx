0cf43e519663475e57686dd0e46069a6f534473e
pankajkoree <pankajkori2057@gmail.com>
2025-03-30 22:24:09 +0530

    auth context

---
+++ src/app/context/AuthContext.tsx
@@ -0,0 +1,68 @@
+"use client";
+import {
+    createContext,
+    useContext,
+    useState,
+    useEffect,
+    ReactNode,
+} from "react";
+
+// Define the shape of the AuthContext
+interface AuthContextType {
+    isLoggedIn: boolean;
+    login: (userData: object) => void;
+    logout: () => void;
+}
+
+// Create the context with the defined type
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const useAuth = (): AuthContextType => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+};
+
+// Define props type for the AuthProvider
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+interface UserData {
+    id: string;
+    email: string;
+    name: string;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+    useEffect(() => {
+        const user = localStorage.getItem("user");
+        if (user) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const login = (userData: UserData) => {
+        if (!userData) {
+            console.error("User data is undefined");
+            return;
+        }
+        localStorage.setItem("user", JSON.stringify(userData));
+        setIsLoggedIn(true);
+    };
+
+    const logout = () => {
+        localStorage.removeItem("user");
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
