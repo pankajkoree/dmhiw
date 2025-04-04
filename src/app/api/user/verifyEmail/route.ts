0b3d056ca66d88bf60d434b9e742472c879a40d0
pankajkoree <pankajkori2057@gmail.com>
2025-04-04 13:05:32 +0530

    verify email route api

---
+++ src/app/api/user/verifyEmail/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connect } from "@/dbConfig/dbConfig";
+import User from "../../../../models/userMomdel";
+
+export const POST = async (request: NextRequest) => {
+  try {
+    await connect();
+
+    const reqBody = await request.json();
+    const { token } = reqBody;
+
+    if (!token) {
+      return NextResponse.json({ error: "Token is required" }, { status: 400 });
+    }
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      console.log("Invalid token or token expired");
+      return NextResponse.json(
+        { error: "Invalid or expired token" },
+        { status: 400 }
+      );
+    }
+
+    user.isVerified = true;
+    user.verifyToken = null;
+    user.verifyTokenExpiry = null;
+
+    await user.save();
+
+    return NextResponse.json(
+      { message: "Email verified successfully", success: true },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error verifying email:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
