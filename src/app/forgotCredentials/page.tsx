5262ef456dd4b2916b981b6c7c907f2de859bbf2
pankajkoree <pankajkori2057@gmail.com>
2025-04-06 23:12:53 +0530

    forgot page

--- src/app/forgotCredentials/page.tsx
+++ src/app/forgotCredentials/page.tsx
@@ -7,9 +7,16 @@ const forgotCredentials = () => {
 
   return (
     <div>
+      <p>Please enter your valid email to reset your password</p>
       <form onSubmit={handleSubmit}>
+
         <label htmlFor="email">Email</label>
         <input type="text" id="email" />
+
+        <br />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
 
         <button type="submit">Change password</button>
       </form>
