0cbfe0ab0d20a60f052e32d350d588626103087a
pankajkoree <pankajkori2057@gmail.com>
2025-04-09 22:55:55 +0530

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="relative flex flex-col justify-center items-center text-center">
       <h1 className="uppercase text-3xl">Digital Mental Health Interventions and Wellness</h1>
       <h2 className="text-xl">Find your calm, anonymously. Explore deeper, personally</h2>
-      <p className="xl:w-[40%]">Discover a safe and anonymous space to explore mental health resources  and begin your wellness journey. When you're ready for deeper insights  and personalized guidance, unlock the pro version to connect your unique challenges  with tailored information, all while maintaining the privacy you value</p>
+      <p className="xl:w-[40%]">Discover a safe and anonymous space to explore mental health resources  and begin your wellness journey. When you are ready for deeper insights  and personalized guidance and  unlock the pro version to connect your unique challenges  with tailored information, all while maintaining the privacy you value</p>
       <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
