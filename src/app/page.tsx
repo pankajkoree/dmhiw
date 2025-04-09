41ed8ee4e01387678242eac4614d513e2da0d005
pankajkoree <pankajkori2057@gmail.com>
2025-04-09 21:08:40 +0530

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
-    <div className="relative flex justify-center items-center text-4xl">
-      <h1>Landing page</h1>
+    <div className="relative flex flex-col justify-center items-center text-center">
+      <h1 className="uppercase text-3xl">Digital Mental Health Interventions and Wellness</h1>
+      <h2 className="text-xl">Find your calm, anonymously. Explore deeper, personally</h2>
+      <p className="xl:w-[40%]">Discover a safe and anonymous space to explore mental health resources  and begin your wellness journey. When you're ready for deeper insights  and personalized guidance, unlock the pro version to connect your unique challenges  with tailored information, all while maintaining the privacy you value</p>
       <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
