1bce72f09f5b08e58a6f99032a6bad86a210191c
pankajkoree <pankajkori2057@gmail.com>
2025-04-18 12:01:59 +0530

    video route

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -2,21 +2,189 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-3xl font-bold mb-4 text-blue-700">Mental health video resources</h1>
-
-        <iframe
-          width="1080"
-          height="720"
-          src="https://www.youtube.com/embed/MFyEwdpC5pM?si=WvrvWpkJ4crROs1P"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+    <div className="relative flex flex-col justify-center items-center">
+      <div>
+        <h1 className="relative flex xl:text-4xl top-4">Videos resources</h1>
       </div>
-    </div>
+
+      <div>
+        <div className="relative top-4 xl:top-8  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center p-2 xl:p-0">
+          {/* Card 1 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/kCQim9VrnDY"
+              title="Dr. Laurie Santos' 5 Favorite Coping Tips"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Dr. Laurie Santos' 5 Favorite Coping Tips</h3>
+          </div>
+          {/* Card 1 - Video content route */}
+
+
+          {/* Card 2 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/H9B5mYfBPlY?si=hdUU87kpgNl8b58V"
+              title="Dr. Gabor Maté on Trauma, Addiction, Ayahuasca, and More"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Dr. Gabor Maté on Trauma, Addiction, Ayahuasca, and More</h3>
+          </div>
+          {/* Card 2 - Video content route */}
+
+          {/* Card 3 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/yPXhoQRTP9Q?si=3To6msWHNj7hV_-z"
+              title="The Frontiers of Psychedelics | Michael Pollan"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">The Frontiers of Psychedelics | Michael Pollan</h3>
+          </div>
+          {/* Card 3 - Video content route */}
+
+
+          {/* Card 4 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/Cp8G5xj0gXM?si=xY63presXGzurMta"
+              title="Instantly Feel at Ease Amidst High Stress"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Instantly Feel at Ease Amidst High Stress</h3>
+          </div>
+          {/* Card 4 - Video content route */}
+
+          {/* Card 5 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/_SQ_myzmV_Q?si=jdNCyE4-TTudEVqt"
+              title="From Homeless and Broke to Top Angel Investor (Uber, SpaceX, and 100+ More)"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">From Homeless and Broke to Top Angel Investor (Uber, SpaceX, and 100+ More)</h3>
+          </div>
+          {/* Card 5 - Video content route */}
+
+
+          {/* Card 6 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/8G9gvqqz4dM?si=97CWTzUgkrIjBY2u"
+              title="Dear Love, What Would You Have Me Know Today?"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Dear Love, What Would You Have Me Know Today?</h3>
+          </div>
+          {/* Card 6 - Video content route */}
+
+          {/* Card 7 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/NkAjdli9JeM?si=SgocSHgd7oLbEr1Y"
+              title="Life Lessons from a Snow Globe / Calm it Down Podcast"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Life Lessons from a Snow Globe / Calm it Down Podcast</h3>
+          </div>
+          {/* Card 7 - Video content route */}
+
+
+          {/* Card 8 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/rs6Q_o-CJds?si=79NXQDaCVDM1yeuL"
+              title="The Mindbody Syndrome With Steve Ozanich"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">The Mindbody Syndrome With Steve Ozanich</h3>
+          </div>
+          {/* Card 8 - Video content route */}
+
+          {/* Card 9 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/pGU842weADs?si=jeGyXGptFMkROI4s"
+              title="Create Your Own Safety Strength and Peace Easily With These Affirmations MAKE YOUR BRAIN STRONG"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Create Your Own Safety Strength and Peace Easily With These Affirmations MAKE YOUR BRAIN STRONG</h3>
+          </div>
+          {/* Card 9 - Video content route */}
+
+          {/* Card 10 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/WhvVHEsid1c?si=Sd3r5mTQuoUMfUoU"
+              title="How to Let go of the Past: Move On, Accept, Forgive, Be Present, Make Peace"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">How to Let go of the Past: Move On, Accept, Forgive, Be Present, Make Peace</h3>
+          </div>
+          {/* Card 10 - Video content route */}
+
+          {/* Card 11 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/SCuXwFQMp0k?si=-MwOSAENKtWiILeP"
+              title="How to Love a Difficult Person | Loving Kindness Meditation"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">How to Love a Difficult Person | Loving Kindness Meditation</h3>
+          </div>
+          {/* Card 11 - Video content route */}
+
+          {/* Card 12 - Video content route */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="240"
+              src="https://www.youtube.com/embed/s6U3vDtdzrE?si=_uBYjCtv-nvb7bb2"
+              title="30 Minute Morning Exercise Routine - Do This Every Day"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">30 Minute Morning Exercise Routine - Do This Every Day</h3>
+          </div>
+          {/* Card 12 - Video content route */}
+
+        </div>
+      </div>
+    </div >
   );
 };
 
