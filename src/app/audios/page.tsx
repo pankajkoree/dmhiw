258c0de2166310f5dc1a623a3d57805e26ab5e0b
pankajkoree <pankajkori2057@gmail.com>
2025-04-18 13:08:39 +0530

    audio resources

--- src/app/audios/page.tsx
+++ src/app/audios/page.tsx
@@ -2,137 +2,201 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-3xl font-bold mb-4 text-purple-700">
-          Audio resources for mental health wellness
-        </h1>
+    <div className="relative flex flex-col justify-center items-center">
+      <div>
+        <h1 className="relative flex xl:text-4xl top-4">Audio resources</h1>
+      </div>
 
-        <div className="border border-green-400 p-2">
-          <iframe
-            width="720"
-            height="480"
-            src="https://www.youtube.com/embed/VgdAcENXy84?si=70mtdPQ5Y5Ahrzfa"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
+      <div>
+        <div className="relative top-4 xl:top-8  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center p-2 xl:p-0">
+
+          {/* audio 1 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/436-facing-horrific-family-trauma-finding-joy-after/id1094120819?i=1000703163017"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Facing Horrific Family Trauma & Finding Joy After Divorce</h3>
+          </div>
+          {/* audio 1 */}
+
+
+          {/* audio 2 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/430-the-truth-about-recovery-with-dr-mala/id1094120819?i=1000689970986"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">The Truth About Recovery with Dr. Mala
+            </h3>
+          </div>
+          {/* audio 2 */}
+
+          {/* audio 3 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/429-coping-with-the-cruel-world-enduring-heartbreak/id1094120819?i=1000687503959"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Coping with the Cruel World & Enduring Heartbreak
+            </h3>
+          </div>
+          {/* audio 3 */}
+
+
+          {/* audio 4 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/426-how-to-find-purpose-and-not-regret-your-life/id1094120819?i=1000683472521"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">How to Find Purpose and Not Regret Your Life with Jordan Grumet
+            </h3>
+          </div>
+          {/* audio 4 */}
+
+
+          {/* audio 5 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/421-grieving-your-therapist-non-suicidal-self-injury/id1094120819?i=1000677087216"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Grieving your Therapist & Non-Suicidal Self-Injury
+            </h3>
+          </div>
+          {/* audio 5 */}
+
+
+          {/* audio 6 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/418-healing-childhood-ptsd-with-anna-runkle-the/id1094120819?i=1000673577555"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Healing Childhood PTSD with Anna Runkle The Crappy Childhood Fairy
+            </h3>
+          </div>
+          {/* audio 6 */}
+
+          {/* audio 7 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/409-are-you-afraid-of-breathing/id1094120819?i=1000664818336"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Are You Afraid of Breathing?
+            </h3>
+          </div>
+          {/* audio 7 */}
+
+
+          {/* audio 8 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/395-overcoming-agoraphobia-common-exposure-mistakes/id1094120819?i=1000653709840"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Overcoming Agoraphobia, Common Exposure Mistakes, Amygdala Reactivity, and More
+            </h3>
+          </div>
+          {/* audio 8 */}
+
+
+          {/* audio 9 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/tune-into-your-fears/id1663877777?i=1000703848217"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Tune Into Your Fears | Meditation for anxiety
+            </h3>
+          </div>
+          {/* audio 9 */}
+
+
+          {/* audio 10 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/a-daily-meditation-practice-to-keep-you-present/id1663877777?i=1000703252669"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">A Daily Meditation Practice to Keep You Present and Positive
+            </h3>
+          </div>
+          {/* audio 10 */}
+
+
+          {/* audio 11 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/take-risks-were-all-learning-here-with-zelina-chinwoh/id899009517?i=1000703456024"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Take Risks, We're All Learning Here with Zelina Chinwoh
+            </h3>
+          </div>
+          {/* audio 11 */}
+
+          {/* audio 12 */}
+          <div className="w-full max-w-[500px] flex flex-col text-center xl:shadow-xl xl:shadow-blue-300 border border-green-300 hover:shadow-lg hover:shadow-green-500 p-4 rounded-xl hover:cursor-pointer">
+
+            <iframe
+              className="w-full rounded-lg"
+              height="175"
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.podcasts.apple.com/us/podcast/healing-from-childhood-trauma-your-questions-answered/id1509439205?i=1000701952089"
+            ></iframe>
+            <h3 className="mt-2 text-lg font-semibold">Healing From Childhood Trauma: Your Questions Answered
+            </h3>
+          </div>
+          {/* audio 12 */}
+
+
         </div>
-
-        <div className="relative flex border border-green-400 p-2 top-4">
-          <iframe
-            width="720"
-            height="480"
-            src="https://www.youtube.com/embed/CJIXbibQ0jI?si=cr8r3F2azhBbCRmb"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
-        </div>
-
-        <p className="flex py-6">
-          If you&apos;re looking to store audio clips on cloud platforms and
-          want to use a free plan here are a few popular ones that offer free
-          tiers:
-        </p>
-        <ul>
-          <li>
-            <strong>Google Drive</strong>
-            <ul>
-              <li>
-                <strong>Free Tier:</strong> 15 GB of storage for free.
-              </li>
-              <li>
-                You can upload and store audio files and access them from
-                anywhere.
-              </li>
-              <li>
-                If you exceed the free storage limit. you&apos;ll need to pay
-                for additional space.
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>Dropbox</strong>
-            <ul>
-              <li>
-                <strong>Free Tier:</strong> 2 GB of storage.
-              </li>
-              <li>
-                Dropbox provides easy-to-use file syncing and sharing but the
-                free tier has limited storage.
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>OneDrive (Microsoft)</strong>
-            <ul>
-              <li>
-                <strong>Free Tier:</strong> 5 GB of storage.
-              </li>
-              <li>
-                OneDrive offers good integration with Microsoft Office apps and
-                the free plan provides basic file storage.
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>iCloud (Apple)</strong>
-            <ul>
-              <li>
-                <strong>Free Tier:</strong> 5 GB of storage.
-              </li>
-              <li>
-                Ideal if you&apos;re in the Apple ecosystem and need to store
-                your audio files for access across devices.
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>Box</strong>
-            <ul>
-              <li>
-                <strong>Free Tier:</strong> 10 GB of storage.
-              </li>
-              <li>
-                Provides file storage and sharing with some collaboration
-                features as well.
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>Amazon S3 (AWS)</strong>
-            <ul>
-              <li>
-                <strong>Free Tier:</strong> 5 GB of standard storage for the
-                first 12 months.
-              </li>
-              <li>
-                Amazon S3 offers more flexibility and scalability but you might
-                need to upgrade after the free tier expires.
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>pCloud</strong>
-            <ul>
-              <li>
-                <strong>Free Tier:</strong> 10 GB of storage.
-              </li>
-              <li>
-                pCloud allows easy file sharing and management with the option
-                to upgrade for more features and storage.
-              </li>
-            </ul>
-          </li>
-        </ul>
-        <p>
-          These platforms are good for storing audio clips with varying levels
-          of storage and functionality. For larger audio collections or
-          professional uses you might need to consider paid options.
-        </p>
       </div>
     </div>
   );
