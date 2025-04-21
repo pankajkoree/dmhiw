409e08fd3308e545a078fbd1a42ce06e0363a896
pankajkoree <pankajkori2057@gmail.com>
2025-04-21 11:04:45 +0530

    forgot credentials

--- src/app/forgotCredentials/page.tsx
+++ src/app/forgotCredentials/page.tsx
@@ -3,11 +3,10 @@
 import { useState } from "react";
 
 const ForgotCredentials = () => {
-
   const [data, setData] = useState({
     email: "",
     password: "",
-  })
+  });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -17,17 +16,26 @@ const ForgotCredentials = () => {
     <div>
       <p>Please enter your valid email to reset your password</p>
       <form onSubmit={handleSubmit}>
-
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
+        <input
+          type="text"
+          id="email"
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
 
         <br />
         <br />
 
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
+        <input
+          type="password"
+          id="password"
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
 
-        <button type="submit">Change password</button>
+        <button type="submit">Update password</button>
       </form>
     </div>
   );
