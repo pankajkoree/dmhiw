ad5082ec62a05997138a1f0e6fd9906b42d2d8b6
pankajkoree <pankajkori2057@gmail.com>
2025-05-05 00:01:18 +0530

    forget credentials api

--- src/app/api/user/forgotPassword/route.ts
+++ src/app/api/user/forgotPassword/route.ts
@@ -1,23 +1,33 @@
 import User from "../../../../models/userMomdel";
 import { connect } from "../../../../dbConfig/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
 
 export const POST = async (request: NextRequest) => {
   try {
     await connect();
 
     const reqBody = await request.json();
-    const { email } = reqBody;
+    const { email, password } = reqBody;
 
-    const user = await User.findOne({ email: email.toLowerCase() });
+    const salt = await bcryptjs.genSalt(10);
+    const newPassword = await bcryptjs.hash(password, salt);
+
+    let user = await User.findOne({ email: email.toLowerCase() });
     if (!user) {
       return NextResponse.json(
         { error: "User doesn't exist" },
         { status: 400 }
       );
     }
-
-    return NextResponse.json(user);
+    if (user) {
+      user = { ...user, password: newPassword };
+      return NextResponse.json({
+        message: "Password reset successfully",
+        success: true,
+        user,
+      });
+    }
   } catch (error) {
     return NextResponse.json(
       {
